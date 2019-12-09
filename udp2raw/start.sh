0beaca56fb59c7ea78355538f0b4af9af477537d
蒋林国 <jianglinguo@eissoft.com>
2019-12-09 15:42:20 +0800

    add some file

---
+++ udp2raw/start.sh
@@ -0,0 +1,8 @@
+pathx=echo $PATH|grep "/sbin"
+if [ “” = “$pathx” ]; then
+   export PATH=$PATH:/sbin
+   echo “no sbin”
+fi
+
+
+/root/udp/udp2raw_amd64 -s -l 0.0.0.0:27015 -r 127.0.0.1:27010 -k maintell --raw-mode faketcp --cipher-mode xor --auth-mode simple -a >> /root/udp/log.txt 2>&1 &
