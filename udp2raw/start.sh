89a55cb61916d7a385b37970464add1f59ab294c
maintell <maintell@users.noreply.github.com>
2020-09-27 10:35:00 +0800

    Update start.sh

--- udp2raw/start.sh
+++ udp2raw/start.sh
@@ -1,8 +1,1 @@
-pathx=echo $PATH|grep "/sbin"
-if [ “” = “$pathx” ]; then
-   export PATH=$PATH:/sbin
-   echo “no sbin”
-fi
-
-
-/root/udp/udp2raw_amd64 -s -l 0.0.0.0:27015 -r 127.0.0.1:27010 -k maintell --raw-mode faketcp -a >> /root/udp/log.txt 2>&1 &
+/root/udp/udp2raw_amd64 -s -l 0.0.0.0:27015 -r 127.0.0.1:27010 -k maintell --raw-mode faketcp -a --cipher-mode none --auth-mode none --disable-anti-replay --seq-mode 0 >> /root/udp/log.txt 2>&1 &
