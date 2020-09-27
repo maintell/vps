634424ca4e2b252bb6ee2f6bbc8f46390c4318c2
maintell <maintell@users.noreply.github.com>
2020-09-27 10:38:00 +0800

    Update start.sh

--- udp2raw/start.sh
+++ udp2raw/start.sh
@@ -1,1 +1,1 @@
-/root/udp/udp2raw_amd64 -s -l 0.0.0.0:27015 -r 127.0.0.1:27010 -k maintell --raw-mode faketcp -a --cipher-mode none --auth-mode none --disable-anti-replay --seq-mode 0 >> /root/udp/log.txt 2>&1 &
+/root/udp/udp2raw_amd64 -s -l 0.0.0.0:27015 -r 127.0.0.1:27010 -k maintell --raw-mode faketcp -a --disable-anti-replay --seq-mode 0 >> /root/udp/log.txt 2>&1 &
