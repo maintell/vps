b54cb8ef3df7a798fb9bafcac4a2429f6a344151
maintell <maintell@users.noreply.github.com>
2023-03-02 15:24:05 +0800

    Create install.sh

---
+++ naive/install.sh
@@ -0,0 +1,4 @@
+yum install epel-release -y
+yum install go
+go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
